5148dc5ec5a70ec23c27c7bef802ee0192262e75
codewreaker <israel.agyeman.prempeh@gmail.com>
2025-12-15 02:09:02 +0000

    chore(feat): deploying on vercel

---
+++ .output/server/chunks/_/rolldown-runtime.mjs
@@ -0,0 +1,13 @@
+import { createRequire } from "node:module";
+var __defProp = Object.defineProperty;
+var __export = (all, symbols) => {
+	let target = {};
+	for (var name in all) __defProp(target, name, {
+		get: all[name],
+		enumerable: true
+	});
+	if (symbols) __defProp(target, Symbol.toStringTag, { value: "Module" });
+	return target;
+};
+var __require = /* @__PURE__ */ createRequire(import.meta.url);
+export { __require as n, __export as t };
