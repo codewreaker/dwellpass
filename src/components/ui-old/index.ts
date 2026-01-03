3efcb334e725f54c25e72e232830b5a4a907c76d
codewreaker <israel.agyeman.prempeh@gmail.com>
2026-01-03 20:17:11 +0000

    chore(feat): adding tailwind  tweaks ready

--- src/components/ui-old/index.ts
+++
@@ -1,7 +0,0 @@
-// UI Components Index - Base UI wrapped components
-export { Button, type ButtonProps, type ButtonVariant, type ButtonSize } from './Button.js';
-export { Input, type InputProps } from './Input.js';
-export { Menu, type MenuProps, type MenuItemConfig } from './Menu.js';
-export { Select, type SelectProps, type SelectOption } from './Select.js';
-export { Tooltip, type TooltipPosition } from '../Tooltip/index.js';
-export { default as TooltipDefault } from '../Tooltip/index.js';
